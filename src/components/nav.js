1418f9760942f518e029c08634a210e9f65da4c5
Carlos <carlosbenitez86753@yahoo.com>
2020-12-01 19:07:21 -0500

    changed class to classname

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,17 +2,17 @@ import React from "react"
 
 function Nav() {
     return (
-        <nav class="navbar navbar-expand-sm">
-            <a class="navbar-brand">Carlos J Benitez</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        <nav className="navbar navbar-expand-sm">
+            <a className="navbar-brand">Carlos J Benitez</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+                <span className="navbar-toggler-icon"><i className="fa fa-bars"></i></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="/">About<span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link active" href="/portfolio">Portfolio</a>
-                    <a class="nav-item nav-link active" href="/contact">Contact</a>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link active" href="/">About<span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link active" href="/portfolio">Portfolio</a>
+                    <a className="nav-item nav-link active" href="/contact">Contact</a>
                 </div>
             </div>
         </nav>
