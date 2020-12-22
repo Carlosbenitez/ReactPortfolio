af5eecb839a175fd3723f9659a978e0870b83654
Carlos <carlosbenitez86753@yahoo.com>
2020-12-22 08:04:10 -0500

    added a video page with a test video

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './pages/contact/contact';
 import Home from './pages/home/home'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Portfolio from './pages/portfolio/portfolio';
+import Videos from './pages/videos/videos'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route exact path = {["/", "/ReactPortfolio"]} component={Home}/>
       <Route  path = "/ReactPortfolio/contact" component={Contact}/>
       <Route  path = "/ReactPortfolio/portfolio" component={Portfolio}/>
+      <Route  path = "/ReactPortfolio/videos" component={Videos}/>
       <Footer />
     </Router>
   );
---
+++ src/pages/videos/videos.js
@@ -0,0 +1,44 @@
+import React from "react"
+import "./videosstyle.css"
+import YouTube from 'react-youtube';
+function Videos() {
+    const vidInfo = [
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+        { title: "REO Test", text: "This is a test to make sure I can upload videos here so please enjoy all this text.", site: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", alt: "REO test video" },
+    ]
+    const renderVid = (card, index) => {
+        return (
+            <div className="col-lg-6">
+                <div className="content" key={index}>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src={card.site} allowfullscreen></iframe>
+                </div>
+                <h3>{card.title}</h3>
+                <section>{card.text}</section>
+            </div>
+            </div>
+            
+            
+            )
+    }
+    return (
+        <>
+            <h1>Some videos I made:</h1>
+            <div className="main">
+                <div id="contentback">
+                    {/* card is imported here */}
+                    <div className="row">
+                        {vidInfo.map(renderVid)}
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Videos
