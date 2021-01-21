69123a1b16ac9a89d80819f2e7dd135bdd3052b4
Carlos <carlosbenitez86753@yahoo.com>
2021-01-20 22:57:54 -0500

    Updates

--- js/contactscript.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector(".btn")
-
-button.addEventListener('click', (e) => {
-
-    window.location.href = "mailto:carlosbenitez86753@yahoo.com?subject=" + document.getElementById('Subject').value + "&body=" +
-    document.getElementById('Message').value
-    window.location.href = 'thankyou.html';
-});
